38b7c01a29abb1a80e41bed990233b576cebf09c
shizukani-cp <shizukani.cp@gmail.com>
2026-03-08 06:27:14 +0900

    refactor: use plugin status instead of external loaded_plugins table

--- lua/manager/core/load.lua
+++ lua/manager/core/load.lua
@@ -1,27 +1,29 @@
----@type table<string, boolean>
-local loaded_plugins = {}
-
 ---@param id string
 local function _do_load(manager, id)
-    if loaded_plugins[id] then
+    local plugin = manager.plugins[id]
+    if not plugin then
         return
     end
-    local plugin = manager.plugins[id]
+    if plugin.status == "loaded" then
+        return
+    end
     vim.cmd("packadd " .. plugin.spec.id)
     if type(plugin.spec.config) == "function" then
         plugin.spec.config(manager)
     end
-    loaded_plugins[id] = true
     plugin.status = "loaded"
     manager.logger:info("Successfuly loaded " .. id)
 end
 
 ---@param id string
 local function _load_with_deps_check(manager, id)
-    if loaded_plugins[id] then
+    local plugin = manager.plugins[id]
+    if not plugin then
         return
     end
-    local plugin = manager.plugins[id]
+    if plugin.status == "loaded" then
+        return
+    end
     local dependencies = plugin.spec.dependencies or {}
     local all_deps_installed = true
     ---@type manager.Plugin[]
@@ -57,15 +59,15 @@ end
 
 ---@param id string
 return function(self, id)
-    if loaded_plugins[id] then
-        return
-    end
     local plugin = self.plugins[id]
     if not plugin then
         error("plugin '" .. id .. "' not found, please make sure you did add().")
     end
+    if plugin.status == "loaded" then
+        return
+    end
 
-    if plugin.status == "installed" or plugin.status == "loaded" then
+    if plugin.status == "installed" then
         _load_with_deps_check(self, id)
     elseif plugin.status == "installing" then
         table.insert(plugin.on_installed_callbacks, function()
